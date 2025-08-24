fdbb1c49b096bca55998bcc18af574f36f0990a3
ak-developer78 <ak-dev78@email.com>
2025-08-24 22:04:22 +0530

    Final deployment to new URL

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/website/", //  <-- ADD THIS LINE
+ base: "/my-live-website/",
   plugins: [react()],
 })
