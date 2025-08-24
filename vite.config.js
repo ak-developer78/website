a435d30c9a3c5779737a9a1914080ee9cbc797da
ak-developer78 <ak-dev78@email.com>
2025-08-24 21:28:40 +0530

    Fix build scripts for Vite and configure base path

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // Add this line:
-  base: '/website/', 
+  base: "/website/", //  <-- ADD THIS LINE
   plugins: [react()],
-  base: '/website/',
 })
